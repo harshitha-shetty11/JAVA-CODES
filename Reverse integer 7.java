748650512fffa55a7e1b4f16ae90890fd1a20a2c
harshitha-shetty11 <harshushetty1206@gmail.com>
2025-05-30 15:21:01 +0530

    Add files via upload

---
+++ Reverse integer 7.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverse(int x) {
+        int reversed = 0;
+        int temp = (x < 0) ? -1 : 1; 
+        x = Math.abs(x); 
+        while (x != 0) {
+            int digit = x % 10; 
+            x /= 10; 
+           if (reversed > (Integer.MAX_VALUE - digit) / 10) {
+                return 0; 
+            }
+            reversed = reversed * 10 + digit; 
+        }
+        return reversed * temp;
+        
+    }
+
+}
